9920b59c0402e67614b79678452b02f10d80dd2f
Standupic <satndupic87@gmail.com>
2018-03-01 13:23:38 +0300

    mongo-connect

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const nodeEnv = env.NODE_ENV || 'development';
 
 export default {
 	port: env.PORT || 8080,
-	host: env.HOST || '188.225.25.82',
+	host: env.HOST || '0.0.0.0',
 	 get serverUrl() {
     	return `http://${this.host}:${this.port}`;
   	}
