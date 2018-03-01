4918372ada676c8b2b3e76a0dc15481d84565895
Standupic <satndupic87@gmail.com>
2018-03-01 16:03:21 +0300

    development

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const env = process.env
 
-export const nodeEnv = env.NODE_ENV || 'production';
+export const nodeEnv = env.NODE_ENV || 'development';
 
 export default {
 	port: env.PORT || 8080,
-	host: env.HOST || '188.225.25.82',
+	host: env.HOST || '0.0.0.0',
 	 get serverUrl() {
     	return `http://${this.host}:${this.port}`;
   	}
